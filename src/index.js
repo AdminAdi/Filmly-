dbc6901d7c5e5a2c0ba282e822dae564018dc8fb
Aditya <itsmeaditya3254@gmail.com>
2024-12-17 23:48:32 +0530

    sidebar and navigation completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import App from './App';
 
+const theme = createTheme({});
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <App />
-  
+
+    <ThemeProvider theme={theme}>
+
+        <App />
+
+    </ThemeProvider>
+
 );
 
